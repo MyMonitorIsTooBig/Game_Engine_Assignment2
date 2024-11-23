ffa54386ba1ea07d4eb88b3a9dee796b92d80d46
Andrew <osterandrew2@gmail.com>
2024-11-23 16:27:36 -0500

    added plugin

---
+++ Assets/Script/Plugin/PluginToUnityIntegration.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using CheatPlugin;
+
+public class PluginToUnityIntegration : MonoBehaviour
+{
+
+    [SerializeField] CheatPlugin.CheatPlugin _plugin;
+
+
+    [SerializeField] bulletSO bulletStats;
+    [SerializeField] enemySO enemyStats;
+    [SerializeField] PlayerMovement player;
+
+
+    [SerializeField] bool scoreChange = false;
+    [SerializeField] bool dmgChange = false;
+    [SerializeField] bool moveChange = false;
+    [SerializeField] bool jumpChange = false;
+    [SerializeField] bool enemScoreChange = false;
+    [SerializeField] bool enemSpeedChange = false;
+    [SerializeField] bool enemyDistChange = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        player = GameObject.Find("Player").GetComponent<PlayerMovement>();
+
+        if (scoreChange)
+        {
+            ScoreUI.Instance.score = _plugin.score;
+        }
+        if (dmgChange)
+        {
+            bulletStats.damage = _plugin.damage;
+        }
+        if (moveChange)
+        {
+            player.moveSpeed = _plugin.moveSpeed;
+        }
+        if (jumpChange)
+        {
+            player.jumpHeight = _plugin.jumpHeight;
+        }
+        if (enemScoreChange)
+        {
+            enemyStats._maxScore = _plugin.enemyScore;
+        }
+        if (enemSpeedChange)
+        {
+            enemyStats._speed = _plugin.enemySpeed;
+        }
+        if (enemyDistChange)
+        {
+            enemyStats._distance = _plugin.enemyDistance;
+        }
+
+    }
+
+
+}
